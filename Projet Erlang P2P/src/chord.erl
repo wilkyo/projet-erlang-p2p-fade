b31f8048d7225952b4f064cdea3a340d982b795b
wfrancois45 <wfrancois45@b8489913-01cb-3207-67ed-cde336f589f2>
2013-05-19 16:47:14 +0000

    Ring nettoyé un peu. Chord commencé (débug, surtout) Possibilité de quitter les processus en faisant: name ! exit.

--- Projet Erlang P2P/src/chord.erl
+++ Projet Erlang P2P/src/chord.erl
@@ -7,7 +7,33 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
-%-export([lookup/1,gets/1,puts/2]).
+-export([chord/3]).
+
+chord(NodeId, HashTable, NextId) ->
+	io:format("IN CHORD: ~w -> ~w~n", [target(NodeId), target(NextId)]),
+	receive
+		{lookup, Key} -> io:format("RECEIVED: ~w -> ~w~n", [target(NodeId), Key]);
+		exit -> io:format("EXITING: ~w~n", [target(NodeId)]), exit(target(NodeId));
+		_ -> ok
+	end,
+	chord(NodeId, HashTable, NextId).
+  
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% Gets the Hashed id from our node.
+%% @param Id The Hashed {name, node} of the node
+%% @return The Id
+id({Id, _}) -> Id.
+
+%% Gets the target of the hashed id.<br />
+%% Can be used to send a message to it.
+%% @param Target The tuple {name, node} that has been hashed to Id
+%% @return The target
+target({_, Target}) -> Target.
 
 %Node
 %node(Id,Succ,Table)-> ok.
@@ -26,11 +52,3 @@
 	
 % put/2
 %puts(HashedKey, Data) -> ok.
-
-  
-
-
-%% ====================================================================
-%% Internal functions
-%% ====================================================================
-
