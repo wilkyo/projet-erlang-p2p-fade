9cc5930d52994b583ff62c36ff1b6bcb1ae8fab1
wfrancois45 <wfrancois45@b8489913-01cb-3207-67ed-cde336f589f2>
2013-05-19 15:18:34 +0000

    Amélioration du ring (plus proche de ce qui est attendu.

--- trunk/Projet Erlang P2P/src/ring.erl
+++ trunk/Projet Erlang P2P/src/ring.erl
@@ -13,10 +13,13 @@
 %% Initializes a node getting its successor.
 %% @param Mod The module to call when the ring is finished
 %% @param Fun The function to call when the ring is finished
-%% @param Name The name of the node
-init(Mod, Fun, Name) ->
+%% @param NodeId The structure {id, {name, node}} of the node
+init(Mod, Fun, NodeId) ->
+	{_, {Name, _}} = NodeId,
+	register(Name, self()),
+	HashTable = dict:new(),
 	receive
-		{next, Next} -> hello(Mod, Fun, Next, Name)
+		{next, Next} -> hello(Mod, Fun, NodeId, HashTable, Next)
 	end.
 
 
@@ -24,10 +27,16 @@ init(Mod, Fun, Name) ->
 %% @param Mod The module to call when the ring is finished
 %% @param Fun The function to call when the ring is finished
 %% @param Names The list of names for the nodes of the ring
-launch(Mod, Fun, Names) -> build(Mod, Fun, Names).
+launch(Mod, Fun, Names) ->
+	Hashed = lists:map(fun(T) ->
+							   Couple = {T, node()},
+							   {crypto:sha(term_to_binary(Couple)), Couple}
+					   end, Names),
+	Sorted = lists:sort(Hashed),
+	build(Mod, Fun, Sorted).
 
 
-test() -> launch(chord, chord, ["alice", "dave", "eve", "bob", "charlie"]).
+test() -> launch(chord, chord, [alice, dave, eve, bob, charlie]).
 
 %% ====================================================================
 %% Internal functions
@@ -37,7 +46,7 @@ test() -> launch(chord, chord, ["alice", "dave", "eve", "bob", "charlie"]).
 %% Creates the last node and sets its next to the first node of the ring.
 %% @param Mod The module to call when the ring is finished
 %% @param Fun The function to call when the ring is finished
-%% @param H The Name of the new node
+%% @param H The structure {id, {name, node}} of the new node
 %% @param First The Pid of the first node of the ring
 %% @return The Pid of the new node
 build_aux(Mod, Fun, [H], First) ->
@@ -48,7 +57,7 @@ build_aux(Mod, Fun, [H], First) ->
 %% Creates a node and asks to create the next.
 %% @param Mod The module to call when the ring is finished
 %% @param Fun The function to call when the ring is finished
-%% @param H The Name of the new node
+%% @param H The structure {id, {name, node}} of the new node
 %% @param T The Names of the following nodes
 %% @param First The Pid of the first node of the ring
 %% @return The Pid of the new node
@@ -60,15 +69,15 @@ build_aux(Mod, Fun, [H|T], First) ->
 %% Creates the first node and asks to create the next.
 %% @param Mod The module to call when the ring is finished
 %% @param Fun The function to call when the ring is finished
-%% @param H The Name of the new node
+%% @param H The structure {id, {name, node}} of the new node
 %% @param T The Names of the following nodes
 build(Mod, Fun, [H|T]) ->
 	Pid = spawn(?MODULE, init, [Mod, Fun, H]),
 	Pid ! {next, build_aux(Mod, Fun, T, Pid)},
 	ok.
 
-hello(Mod, Fun, Pid, Name) ->
-	io:format("~w:~w # ~w # ~w -> ~w)~n", [Mod, Fun, Name, self(), Pid]).
+hello(Mod, Fun, NodeId, HashTable, Next) ->
+	io:format("~w:~w # ~w~n~w -> ~w~n", [Mod, Fun, NodeId, self(), Next]).
 
 
 
