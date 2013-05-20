d732fe85eba04b55a767d960009a415f1b1afedf
wfrancois45 <wfrancois45@b8489913-01cb-3207-67ed-cde336f589f2>
2013-05-20 00:11:55 +0000

    Changement structure

---
+++ trunk/jalon1/erl/chord.erl
@@ -0,0 +1,103 @@
+%% @author vincent
+%% @doc @todo Add description to chord.
+
+
+-module(chord).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([chord/3]).
+
+%%
+chord(NodeId, HashTable, NextId) ->
+	io:format("~w IN CHORD: ~w -> ~w~n", [self(), target(NodeId), target(NextId)]),
+	receive
+		{lookup, Who, Key} ->
+			Who ! lookup(NodeId, NextId, Key),
+			chord(NodeId, HashTable, NextId);
+		{get, Who, Key} ->
+			Who ! get(NodeId, HashTable, NextId, Key),
+			chord(NodeId, HashTable, NextId);
+		{getself, Who, Key} ->
+			Who ! getself(HashTable, Key),
+			chord(NodeId, HashTable, NextId);
+		{put, _, Key, Data} ->
+			NewHashTable = put(NodeId, HashTable, NextId, Key, Data),
+			chord(NodeId, NewHashTable, NextId);
+		{putself, _, Key, Data} ->
+			NewHashTable = putself(HashTable, Key, Data),
+			chord(NodeId, NewHashTable, NextId);
+		exit -> io:format("EXITING: ~w~n", [target(NodeId)]), exit(target(NodeId));
+		_ -> chord(NodeId, HashTable, NextId)
+	end.
+  
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% Gets the Hashed id from our node.
+%% @param Id The Hashed {name, node} of the node
+%% @return The Id
+id({Id, _}) -> Id.
+
+%% Gets the target of the hashed id.<br />
+%% Can be used to send a message to it.
+%% @param Target The tuple {name, node} that has been hashed to Id
+%% @return The target
+target({_, Target}) -> Target.
+
+%%
+lookup(NodeId, NextId, Key) ->
+	X = id(NodeId),
+	Y = id(NextId),
+	if 
+		((Key>X) and (Key<Y)) ->
+		 	NextId;
+		((Key>X) and (Y<X)) ->
+			NextId;
+		true -> target(NextId) ! {lookup, self(), Key},
+				receive
+					Value -> Value
+				end
+	end.
+
+%%
+get(NodeId, HashTable, NextId, Key) ->
+	io:format("get ~w~n", [target(NodeId)]),
+	N = lookup(NodeId, NextId, Key),
+	case N of
+		NodeId -> getself(HashTable, Key);
+		ResponsibleId ->
+			target(ResponsibleId) ! {getself, self(), Key},
+			receive
+				Value -> Value
+			end
+	end.
+
+%%
+getself(HashTable, Key) ->
+	M = dict:find(Key,HashTable),
+	case M of
+	 {ok, Value} -> Value;
+	 _ -> io:format("KEY NOT FOUND")
+	end.
+	
+
+%%
+put(NodeId, HashTable, NextId, Key, Data) ->
+	N = lookup(NodeId,NextId,Key),
+	case N of
+		NodeId -> putself(HashTable, Key, Data);
+		ResponsibleId ->
+			target(ResponsibleId) ! {putself, self(), Key, Data},
+			HashTable
+	end.
+
+%%
+putself(HashTable, Key, Data) ->
+	dict:store(Key,Data,HashTable).
+
+
---
+++ trunk/jalon1/gui/ErlangNode.java
@@ -0,0 +1,139 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.image.ImageObserver;
+import java.io.IOException;
+import java.text.AttributedCharacterIterator;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import com.ericsson.otp.erlang.*; 
+
+public class ErlangNode extends OtpNode{
+	
+	OtpMbox mbox;
+	static List<Node> listNode=new ArrayList<Node>();
+	
+	public ErlangNode(String nameNode) throws IOException {
+		super(nameNode);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean ping(String serverName){
+		if (this.ping(serverName,2000)) {
+			System.out.println("remote is up");
+			return true;
+		}
+		else {
+			System.out.println("remote is not up");
+			return false;
+		} 
+	}
+	
+	public OtpErlangPid receive(ErlangNode node,String boxname){
+		//On crée la boîte mail de zou, boxname est la boîte mail du 
+		//sera utiliser pour envoyer le message dans Erlang
+		OtpErlangTuple msg;
+		OtpErlangPid pidfrom;
+		mbox=node.createMbox(boxname);
+		OtpErlangObject objet;
+		while(true){
+			
+			try{
+				//attend l'envoie du message
+				objet= mbox.receive();
+				
+				if(objet instanceof OtpErlangTuple){
+					msg = (OtpErlangTuple)objet;
+					pidfrom = (OtpErlangPid)(msg.elementAt(0));
+					System.out.println("Receive something from {< "+pidfrom+" >, "+msg.elementAt(1)+" }");
+					return pidfrom;
+				}
+			}
+			
+			catch(Exception e){
+				System.out.println("Please check! Exception\n"+ e);
+			}
+		}
+	}
+	
+	public OtpErlangPid receiveList(ErlangNode node,String boxname) throws OtpErlangExit, OtpErlangDecodeException{
+		OtpNode myNode=null;
+		OtpErlangList message;
+		OtpErlangPid pidfrom;
+		mbox=node.createMbox(boxname);
+		OtpErlangObject objet;
+		OtpErlangList list;
+		while(true){
+			objet= mbox.receive();
+			
+			//System.out.println(objet);
+			message=(OtpErlangList) objet;
+			//list=(OtpErlangList) message.elementAt(1); //recupère la liste
+			for(OtpErlangObject elemList: message){
+				OtpErlangTuple idHash_tupleName=(OtpErlangTuple) elemList;
+				OtpErlangBinary code=(OtpErlangBinary) idHash_tupleName.elementAt(0);// code Hashé
+				 
+							OtpErlangTuple name_node=(OtpErlangTuple)idHash_tupleName.elementAt(1); // recupère l'élément associé au code hashé
+							OtpErlangObject nom=(OtpErlangObject) name_node.elementAt(0); //recupère le nom du process
+							
+							System.out.println("code: "+code.hashCode()+" , "+nom.toString());
+							listNode.add(new Node(code.hashCode(),nom.toString()));
+			}
+			return null;
+		}
+		
+	}
+	
+	static List<Node> getList(){
+		return listNode;
+	}
+	
+	public void reply(OtpErlangPid pid){
+		OtpErlangObject[] msgReply = new OtpErlangObject[2];
+		msgReply[0] = mbox.self();
+		msgReply[1] = new OtpErlangAtom("pong");
+		System.out.println("My pid is < "+msgReply[0]+" >");
+		System.out.println("Send message to < "+pid+" >");
+		mbox.send(pid,msgReply[1]);
+	}
+
+	public static void main(String []args) throws Exception{
+		
+		final ErlangNode mynode=new ErlangNode("gui@localhost");
+		mynode.ping("here@localhost"); 
+		OtpErlangPid pid=mynode.receiveList(mynode, "mbox");
+		//NodeView nodeView=new NodeView();
+		
+		EventQueue.invokeLater(new Runnable() {       	
+        	
+            @Override
+            public void run() {
+                try {
+                	JFrame f = new JFrame();
+                    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    f.add(new NodeView(mynode)); // n=5 affichage de n process
+                    f.pack();
+                    f.setVisible(true);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        });
+		
+		
+		
+		
+		//mynode.reply(pid);
+		
+	}
+}
